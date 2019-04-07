f4eca4a9c80fd3f350ae1280ed54bd2d14f19c7a
jumatberkah <redminote5arom@gmail.com>
2019-04-07 15:25:37 +0700

    kalkulator: adapt for python3
    'raw_input()' and several other input method are merged to 'input()' in python3.
    So i changed 'raw_input()' to 'input()

--- kalkulator.py
+++ kalkulator.py
@@ -3,13 +3,13 @@ import sys
 
 def testing():
     print ("Selamat Datang di Program Kalkulator dengan prinsip Kondisional Sederhana")
-    opsimasuk = raw_input("Pilih Opsi: [Mulai/Tidak]")
+    opsimasuk = input("Pilih Opsi: [Mulai/Tidak]")
     if opsimasuk == "Tidak":
         sys.exit()
     elif opsimasuk == "Mulai" or "mulai" :
-        print ("Selamat datang di menu transaksi")
+        print ("Selamat Mencoba! Ikuti perintah yang diberikan!")
         nilai = input("Masukkan angka untuk dioperasikan:")
-        operasi = raw_input("Tulislah operasi yang diinginkan: [tambah/kali/bagi/kurang/pangkat/akar]")
+        operasi = input("Tulislah operasi yang diinginkan: [tambah/kali/bagi/kurang/pangkat/akar]")
         nilaikedua = input("Masukkan Nilai Kedua:")
         if operasi == "tambah":
             hasil = (float(nilai)) + (float(nilaikedua))
